8df8a59d9dd12e5a153ee20cb9697ad8875c30b3
Morgan Young <52981150+missmorganyoung@users.noreply.github.com>
2022-12-13 14:51:21 -0800

    adding the primary loop and its "checks"

--- main.ino
+++ main.ino
@@ -2,3 +2,10 @@
 // Fall 2022
 // Morgan Young & Dylan Perkins
 // Group 404
+
+void loop() {
+  delay(500);
+  float humidity = dht.readHumidity();
+  float tempF = dht.readTemperature(true);
+  int value = analogRead(A5);
+  buttonState = digitalRead(button);
